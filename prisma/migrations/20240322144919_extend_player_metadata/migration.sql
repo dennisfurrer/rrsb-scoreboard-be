a169d4638883880d3e37df64b1def21649997848
Dennis Furrer <dennis.furrer@gmail.com>
2024-03-22 15:51:28 +0100

    feat: extend player metadata with flag and image

---
+++ prisma/migrations/20240322144919_extend_player_metadata/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Match" ADD COLUMN     "player1FlagUrl" TEXT,
+ADD COLUMN     "player1ImageUrl" TEXT,
+ADD COLUMN     "player2FlagUrl" TEXT,
+ADD COLUMN     "player2ImageUrl" TEXT;
